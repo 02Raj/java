a8aac16f5879b3632901f4cd964d936fd5466b0a
02Raj <101422375+02Raj@users.noreply.github.com>
2022-07-05 13:24:04 +0530

    Add files via upload

---
+++ Casting.java
@@ -0,0 +1,12 @@
+class Casting
+{
+    public static void main (String[] args){
+         char let = 'B';
+         int ascii = let;
+         char alph = (char) ascii;
+
+            System.out.println(ascii);
+            System.out.println(alph);
+
+    }
+}
