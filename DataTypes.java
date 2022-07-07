0a2163d7a2cd8ffd48501df976505effa2cfce8e
02Raj <101422375+02Raj@users.noreply.github.com>
2022-07-07 13:45:43 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,16 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        String name = "Paul";
+        String brand = "idol";
+        double processorspeed = 2.66;
+        float ramSize = 8;
+        int HardDisk = 500;
+        byte InchMonitor = 15;
+        double rupeesIn$ = 1760.55;
+        char servicePlan = 'C';
+        boolean OnSiteServicing = true;
+        boolean ExtendedWarranty = false;
+
+    }
+}
