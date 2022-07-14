862865d94443e1bd50fdc7228dc6a5770ee800d9
02Raj <101422375+02Raj@users.noreply.github.com>
2022-07-14 09:03:47 +0530

    Add files via upload

---
+++ Dowhile.java
@@ -0,0 +1,12 @@
+public class Dowhile {
+    public static void main(String arg[])
+    {
+        int i=11;
+        do
+        {
+            System.out.println(i);
+            i--;
+        }
+        while(i>3);
+    }
+}
---
+++ Forloop.java
@@ -0,0 +1,9 @@
+public class Forloop {
+    public static void main(String arg[])
+    {
+        for(int i=10;i>6;i--)
+        {
+            System.out.println(i);
+        }
+    }
+}
