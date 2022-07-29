dcf59e9f82f9ccaadb1adb8e824779b58a18881d
02Raj <101422375+02Raj@users.noreply.github.com>
2022-07-29 18:56:31 +0530

    Add files via upload

---
+++ CheckOverRidingFinal.java
@@ -0,0 +1,20 @@
+//class C
+//{
+//    static void check()
+//    {
+//        System.out.println("Statement 1");
+//    }
+//}
+//class B extends C{
+//    static void check(){
+//        System.out.println("Statement 2");
+//    }
+//}
+//class CheckOverRidingStatic{
+//    public static void main(String[] args)
+//    {
+//        B k = new B();
+//        k.check();
+//
+//    }
+//}
